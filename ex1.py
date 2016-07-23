21b1337056a018272491190c5e7e6e5a63039067
simplejeff <jeffshen86@gmail.com>
2016-07-23 12:53:17 -0700

    [feature] added graph

--- ex1.py
+++ ex1.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+#creates a normal distribution of values centered around 100, std of 20, and 10000 values
+incomes = np.random.normal(100, 20, 10000)
+
+#finds the mean and median of the incomes data set
+print np.mean(incomes)
+print np.median(incomes)
+
+#creates a histogram with 50 values to a bucket
+plt.hist(incomes, 50)
+plt.show()
